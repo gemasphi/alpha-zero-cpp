4a71cd08300105672b8affd48608979437cf6af4
gemasphi <alexandreborges2312@gmail.com>
2020-08-01 21:14:36 +0100

    optimize refactor

--- optimize.py
+++ optimize.py
@@ -2,30 +2,23 @@ import yaml
 import optuna
 from py.NN import NetWrapper
 from train import train_az
+from params import TrainParams,MCTSParams, to_args, instanciate_params_from_args
 
 def objective(trial):
 	lr = trial.suggest_loguniform('lr', 0.005, 0.5)
 	wd = trial.suggest_loguniform('wd', 0.001, 0.05)
 	momentum = trial.suggest_loguniform('momentum', 0.001, 0.05)
 	
-	NN_PARAMS = {
-		"input_planes": 1,
-		"batch_size": 1024,
-		"lr" : lr,
-		"wd" : wd,
-		"momentum" : momentum,
-		"scheduler_params" : {}
-	}
-
-	DATA = {
-		"location": "build/temp/perfect_player/",
-		"n_games": 0.5
-	}
-	return train_az(
-		"temp/models/traced_model_new.pt",
-		"temp/models/", 
-		NN_PARAMS, 
-		DATA)
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, allow_abbrev=False)
+	parser.add_argument('--input_planes', --default=1, type=int)
+	parser.add_argument('--current_gen', --default=1, type=int)
+	TrainParams.add_to_parser(parser)
+	MCTSParams.add_to_parser(parser)
+	args = parser.parse_args()
+	args.lr = lr
+	args.momentum = momentum
+	args.wd = wd
+	return train_az(args)
 
 study = optuna.create_study(direction='minimize')
 study.optimize(objective, n_trials = 100)
